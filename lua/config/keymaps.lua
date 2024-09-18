47d9f34465c94b64c7c2b619b1725d7b050c029a
Srijan Chapagain <srijan.chapagain@wellmo.com>
2024-09-18 12:25:48 +0300

    add key map config for live grep args prompt

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 -- lua/plugins/keymaps.lua
-local live_grep = require("plugins.live-grep")
 local wk = require("which-key")
 
-vim.keymap.set("n", "<leader>s.", require("telescope").extensions.live_grep_args.live_grep_args, { noremap = true })
+wk.add({ "<leader>s.", require("telescope").extensions.live_grep_args.live_grep_args, desc = "Live grep args prompt" })
